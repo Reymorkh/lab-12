e1434512a6234dc0c59e7960139dcf230868bfa5
Reymorkh <Reymorkh@gmail.com>
2024-04-25 14:51:35 +0500

    Добавьте файлы проекта.

---
+++ TestProject1/ListTest.cs
@@ -0,0 +1,214 @@
+using Lab_10;
+using lab_12;
+
+namespace Tests
+{
+  [TestClass]
+  public class ListTest
+  {
+    [TestMethod]
+    public void TestListInsides()
+    {
+      Watch watch1 = new Watch();
+      watch1.RandomInit();
+      Watch watch2 = new Watch();
+      watch2.RandomInit();
+      Watch watch3 = new Watch();
+      watch3.RandomInit();
+      Watch watch4 = new Watch();
+      watch4.RandomInit();
+      DoublyLinkedList<Watch> list = new DoublyLinkedList<Watch>();
+      list.AddLast(watch1);
+      list.AddLast(watch2);
+      list.AddLast(watch3);
+      list.AddLast(watch4);
+      Assert.AreEqual($"{watch1}\n{watch2}\n{watch3}\n{watch4}\n", list.ShowItems());
+    }
+
+    [TestMethod]
+    public void TestListInsidesAfterReset()
+    {
+      Watch watch1 = new Watch();
+      watch1.RandomInit();
+      Watch watch2 = new Watch();
+      watch2.RandomInit();
+      Watch watch3 = new Watch();
+      watch3.RandomInit();
+      Watch watch4 = new Watch();
+      watch4.RandomInit();
+      DoublyLinkedList<Watch> list = new DoublyLinkedList<Watch>();
+      list.AddLast(watch1);
+      list.AddLast(watch2);
+      list.AddLast(watch3);
+      list.AddLast(watch4);
+      list.Reset();
+      Assert.AreEqual(" ÓÎÎÂÍˆËˇ ÔÛÒÚ‡.", list.ShowItems());
+    }
+
+    [TestMethod]
+    public void TestListInsidesAfterResetAndAddition()
+    {
+      Watch watch1 = new Watch();
+      watch1.RandomInit();
+      Watch watch2 = new Watch();
+      watch2.RandomInit();
+      Watch watch3 = new Watch();
+      watch3.RandomInit();
+      Watch watch4 = new Watch();
+      watch4.RandomInit();
+      DoublyLinkedList<Watch> list = new DoublyLinkedList<Watch>();
+      list.AddLast(watch1);
+      list.AddLast(watch2);
+      list.AddLast(watch3);
+      list.AddLast(watch4);
+      list.Reset();
+      list.AddLast(watch1);
+      list.AddLast(watch2);
+      list.AddLast(watch3);
+      list.AddLast(watch4);
+      Assert.AreEqual($"{watch1}\n{watch2}\n{watch3}\n{watch4}\n", list.ShowItems());
+    }
+
+    [TestMethod]
+    public void AdditionMethodsTest()
+    {
+      Watch watch1 = new Watch();
+      watch1.RandomInit();
+      Watch watch2 = new Watch();
+      watch2.RandomInit();
+      Watch watch3 = new Watch();
+      watch3.RandomInit();
+      Watch watch4 = new Watch("adsd", 123);
+
+      DoublyLinkedList<Watch> list = new DoublyLinkedList<Watch>();
+
+      list.AddLast(watch1);
+      list.AddLast(watch2);
+      list.AddLast(watch3);
+      list.Insert(watch4, 1);
+
+      Assert.AreEqual($"{watch1}\n{watch4}\n{watch2}\n{watch3}\n", list.ShowItems());
+    }
+
+    [TestMethod]
+    public void AdditionMethodsToStringTest()
+    {
+      Watch watch1 = new Watch();
+      watch1.RandomInit();
+      Watch watch2 = new Watch();
+      watch2.RandomInit();
+      Watch watch3 = new Watch();
+      watch3.RandomInit();
+      Watch watch4 = new Watch("adsd", 123);
+
+      DoublyLinkedList<Watch> list = new DoublyLinkedList<Watch>();
+
+      list.AddLast(watch1);
+      list.AddLast(watch2);
+      list.AddLast(watch3);
+      list.Insert(watch4, 1);
+
+      Assert.AreEqual($"{watch1}\n{watch4}\n{watch2}\n{watch3}\n", list.ToString());
+    }
+
+    [TestMethod]
+    public void Deletion»Ì“Ù¸ÛMethodsTest()
+    {
+      Watch watch1 = new Watch();
+      watch1.RandomInit();
+      Watch watch2 = new Watch();
+      watch2.RandomInit();
+      Watch watch3 = new Watch();
+      watch3.RandomInit();
+      Watch watch4 = new Watch("adsd", 123);
+
+      DoublyLinkedList<Watch> list = new DoublyLinkedList<Watch>();
+
+      list.AddLast(watch1);
+      list.AddLast(watch2);
+      list.AddLast(watch3);
+      list.Insert(watch4, 1);
+
+      list.DeleteByName("adsd");
+
+      Assert.AreEqual($"{watch1}\n{watch2}\n{watch3}\n", list.ShowItems());
+    }
+    
+
+    [TestMethod]
+    public void DeletionByYearMethodsTest()
+    {
+      Watch watch1 = new Watch();
+      watch1.RandomInit();
+      Watch watch2 = new Watch();
+      watch2.RandomInit();
+      Watch watch3 = new Watch();
+      watch3.RandomInit();
+      Watch watch4 = new Watch("adsd", 123);
+
+      DoublyLinkedList<Watch> list = new DoublyLinkedList<Watch>();
+
+      list.AddLast(watch1);
+      list.AddLast(watch2);
+      list.AddLast(watch3);
+      list.Insert(watch4, 2);
+
+      list.DeleteByYear(123);
+
+      Assert.AreEqual($"{watch1}\n{watch2}\n{watch3}\n", list.ShowItems());
+    }
+    
+    [TestMethod]
+    public void DeleteFirstMethodTest()
+    {
+      Watch watch1 = new Watch();
+      watch1.RandomInit();
+      Watch watch2 = new Watch();
+      watch2.RandomInit();
+      Watch watch3 = new Watch();
+      watch3.RandomInit();
+      Watch watch4 = new Watch("adsd", 123);
+
+      DoublyLinkedList<Watch> list = new DoublyLinkedList<Watch>();
+
+      list.AddLast(watch1);
+      list.AddLast(watch2);
+      list.AddLast(watch3);
+      list.Insert(watch4, 1);
+
+      list.DeleteFirst();
+
+      Assert.AreEqual($"{watch4}\n{watch2}\n{watch3}\n", list.ShowItems());
+    }
+
+    [TestMethod]
+    public void AddFirstMethodTest()
+    {
+      Watch watch1 = new Watch();
+      watch1.RandomInit();
+      Watch watch2 = new Watch();
+      watch2.RandomInit();
+      Watch watch3 = new Watch();
+      watch3.RandomInit();
+      Watch watch4 = new Watch("adsd", 123);
+
+      DoublyLinkedList<Watch> list = new DoublyLinkedList<Watch>();
+
+      list.AddFirst(watch1);
+      list.AddFirst(watch2);
+      list.AddFirst(watch3);
+      list.AddFirst(watch4);
+
+      Assert.AreEqual($"{watch4}\n{watch3}\n{watch2}\n{watch1}\n", list.ShowItems());
+    }
+
+
+
+
+
+
+
+
+
+  }
+}
---
+++ lab-12/HTable.cs
@@ -0,0 +1,73 @@
+﻿
+
+
+namespace lab_12
+{
+  internal class HTable<T>
+  {
+    public Node<T>[]? table;
+    public int Size { get; set; }
+
+    public HTable(int size)
+    {
+      Size = size;
+      table = new Node<T>[Size];
+    }
+
+    public bool Add(T item)
+    {
+      Node<T> point = new Node<T>(item);
+      if (item == null)
+        return false;
+      int index = Math.Abs(point.GetHashCode()) % Size;
+      if (table[index] == null)
+        table[index] = point;
+      else
+      {
+        Node<T> cur = table[index];
+        if (string.Compare(cur.ToString(), point.ToString()) == 0)
+          return false;
+        while (cur.Next != null)
+        {
+          if (string.Compare(cur.ToString(), point.ToString()) == 0)
+            return false;
+          cur = cur.Next;
+        }
+        cur.Next = point;
+      }
+      return true;
+    }
+
+    public Node<T> this[int index] { get => table[index] == null ? throw new Exception("") : table[index]; }
+
+
+
+  }
+
+  class Node<T>
+  {
+    public int key;
+    public T? Value { get; set; }
+
+    public Node<T>? Next { get; set; }
+    public Node(T item)
+    {
+      Value = item;
+      key = GetHashCode();
+      Next = null;
+    }
+
+    public override string ToString()
+    {
+      return Value == null ? "0" : key + ":" + Value.ToString();
+    }
+    public override int GetHashCode()
+    {
+      return Value == null ? 0 : Value.GetHashCode();
+    }
+
+
+
+
+  }
+}
